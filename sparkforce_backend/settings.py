94dfac2aca4a150bdecd739b8cab4d9ca80c302f
Ahmad Khidir <khidirahmad05@gmail.com>
2022-05-20 16:53:00 +0100

    allowed host updated

--- sparkforce_backend/settings.py
+++ sparkforce_backend/settings.py
@@ -27,12 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'sparkforce-backend.herokuapp.com'
+    'sparkforce-backend.herokuapp.com',
+    '127.0.0.1',
+    'joinsparkforce.netlify.app/'
 ]
 
 CORS_ALLOWED_ORIGINS = [
--- v1/views.py
+++ v1/views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.renderers import AdminRenderer, JSONRenderer
 
 from .serializers import WaitlistSubsrcibersSerializers
 
@@ -10,3 +11,4 @@ class WaitlistSubscribersListView(ListCreateAPIView):
     queryset = WaitlistSubscribers.objects.all()
     serializer_class = WaitlistSubsrcibersSerializers
     permission_classes = [IsAuthenticated]
+    renderer_classes = [AdminRenderer]
