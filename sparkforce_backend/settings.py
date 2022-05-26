e9fb80fdd1ed332eb8fcf1fcfcd32de8a18af2fb
Ahmad Khidir <khidirahmad05@gmail.com>
2022-05-26 17:28:49 +0100

    CORS allowed origins updated

--- sparkforce_backend/settings.py
+++ sparkforce_backend/settings.py
@@ -39,7 +39,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
     "https://joinsparkforce.netlify.app",
-    "http://www.joinsparkforce.com"
+    "https://www.joinsparkforce.com"
 ]
 
 # Application definition
